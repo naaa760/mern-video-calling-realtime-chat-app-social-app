550c24238f8b29e274d5e2c920b04e777081b509
nehhhhha6 <n0370525@gmail.com>
2025-04-30 16:46:38 +0530

    update

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -31,7 +31,7 @@ const SignUpPage = () => {
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="size-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Streamify
+              BeamUp
             </span>
           </div>
 
@@ -48,7 +48,7 @@ const SignUpPage = () => {
                 <div>
                   <h2 className="text-xl font-semibold">Create an Account</h2>
                   <p className="text-sm opacity-70">
-                    Join Streamify and start your language learning adventure!
+                    Join BeamUp and start your language learning adventure!
                   </p>
                 </div>
 
